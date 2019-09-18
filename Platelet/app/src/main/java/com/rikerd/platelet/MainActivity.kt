041bb9e4263b045cf4a20cfcac74bfc8672cd918
Rikerd <cursedmarkedsasuke@gmail.com>
2019-09-18 14:23:20 -0700

    Navigation Bar
    Update navigation bar to be immersive

--- Platelet/app/src/main/java/com/rikerd/platelet/MainActivity.kt
+++ Platelet/app/src/main/java/com/rikerd/platelet/MainActivity.kt
@@ -20,8 +20,18 @@ class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
